9a9804aa720e5dd69074ed6cfd79385335c28eb2
aminakhatun22 <aminak26802@gmail.com>
2022-11-09 16:39:08 +0600

    added navbar

--- src/Pages/Navigation/Navigation.js
+++ src/Pages/Navigation/Navigation.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import navIcon from "../../assests/images/footer-gallery/food-safety.png";
+
 
 const Navigation = () => {
     return (
@@ -10,36 +13,29 @@ const Navigation = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
+
+                            <li><Link to="/home">Home</Link></li>
+                            <li><Link href='/blog'>Blog</Link></li>
                             <li tabIndex={0}>
-                                <a className="justify-between">
-                                    Parent
-                                    <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                                </a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
+                                <Link to="/review">Review</Link>
                             </li>
-                            <li><a>Item 3</a></li>
+                            <li><Link to="">My Reviews</Link></li>
+                            <li><Link to="">Add Service</Link></li>
+                            <li><Link to="">Login</Link></li>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <img className='object-scale-down h-20 w-46 rounded' src={navIcon}></img>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal p-0">
-                        <li><a href='/blog'>Blog</a></li>
-                        <li tabIndex={0}>
-                            <a>
-                                Parent
-                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                            </a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
+                        <li><Link href='/home'>Home</Link></li>
+                        <li><Link href='/blog'>Blog</Link></li>
+                        <li >
+                            <Link to="/review">Review</Link>
                         </li>
-                        <li><a>Item 3</a></li>
+                        <li><Link to="">My Reviews</Link></li>
+                        <li><Link to="">Add Service</Link></li>
+                        <li><Link to="">Login</Link></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
@@ -51,3 +47,5 @@ const Navigation = () => {
 };
 
 export default Navigation;
+
+
